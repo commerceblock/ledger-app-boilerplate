e5bd63c1407ee45ba2360ee4d0584dbb3ba4440b
Lawrence Deacon <lawrence.deacon@gmail.com>
2020-04-06 12:15:50 +0100

    Correct load_dgls.sh

--- load_dgld.sh
+++ load_dgld.sh
@@ -2,6 +2,8 @@
 ./docker-run.sh
 rm -rf apploader/load_dgld
 docker cp ledger-dgld-app:/apploader/load_dgld apploader
+rm -rf apploader/load_bitcoin
+docker cp ledger-dgld-app:/apploader/load_bitcoin apploader
 cd apploader
 ./load_dgld.sh
 cd -
